da0bfad7efa233bc362686cc32942344cb4c0712
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-11-24 15:49:29 -0600

    Prueba3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // Si vas a usar usuario.github.io (repo con nombre = username.github.io) usa base: '/'
 export default defineConfig({
-  base: '/myPortfolio/', 
+  base: './', 
   plugins: [react()]
 })
