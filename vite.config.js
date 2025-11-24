374b00341d1bc4c939476e786fb258fd23fc976e
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-11-24 15:49:13 -0600

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Si vas a usar usuario.github.io (repo con nombre = username.github.io) usa base: '/'
-export default defineConfig({
-  base: '/myPortfolio/', 
-  plugins: [react()]
-})
