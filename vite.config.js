b613c61f9d96edf3c442fc18a99b86e46e9ef2be
Steven Antonio Piedra Villalta <steven.piedra02@gmail.com>
2025-11-24 15:24:05 -0600

    Prueba 1

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// REEMPLAZA '/repo-name/' por el nombre de tu repo (con slash inicial y final)
 // Si vas a usar usuario.github.io (repo con nombre = username.github.io) usa base: '/'
 export default defineConfig({
-  base: '/MyPortfolio/', 
+  base: '/myPortfolio/', 
   plugins: [react()]
 })
